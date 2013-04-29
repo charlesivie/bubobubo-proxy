e0ecd731b7bcb7dd3b4d3f35f433552dbdddc28a
Philip Coates <philip.coates@epeirogenic.com>
2013-04-29 17:43:14 +0100

    Added

---
+++ src/main/java/uk/co/bubobubo/service/RepositoryIdExtractor.java
@@ -0,0 +1,22 @@
+package uk.co.bubobubo.service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RepositoryIdExtractor {
+
+	private static final String DEFAULT_REPOSITORY_REGEX = "/repositories/([^/]+)/?";
+
+	public String extractRepositoryId(String path) {
+
+		Pattern p = Pattern.compile(DEFAULT_REPOSITORY_REGEX);
+		Matcher m = p.matcher(path);
+
+		try {
+			m.find();
+			return m.group(1);
+		} catch(Exception e) {
+			return "NOT ALLOWED";
+		}
+	}
+}
