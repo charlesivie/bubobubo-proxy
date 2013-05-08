e925b7a01a074b806f63a1f7e18dad257759ff63
Philip Coates <philip.coates@epeirogenic.com>
2013-05-08 14:00:43 +0100

    Switched to newer non-deprecated version of PasswordEncoder

--- src/main/java/uk/co/bubobubo/security/TwoWayPasswordEncoder.java
+++ src/main/java/uk/co/bubobubo/security/TwoWayPasswordEncoder.java
@@ -1,9 +1,9 @@
 package uk.co.bubobubo.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,15 +16,15 @@ public class TwoWayPasswordEncoder implements PasswordEncoder {
 	private String encryptionSalt;
 
 	@Override
-	public String encodePassword(String password, Object ignoredSalt) {
+	public String encode(CharSequence password) {
 		TextEncryptor textEncryptor = Encryptors.queryableText(encryptionKey, encryptionSalt);
-		return textEncryptor.encrypt(password);
+		return textEncryptor.encrypt(password.toString());
 	}
 
 	@Override
-	public boolean isPasswordValid(String encryptedPassword, String rawPassword, Object ignoredSalt) {
+	public boolean matches(CharSequence rawPassword, String encryptedPassword) {
 		TextEncryptor textEncryptor = Encryptors.queryableText(encryptionKey, encryptionSalt);
-		return encryptedPassword.equals(textEncryptor.encrypt(rawPassword));
+		return encryptedPassword.equals(textEncryptor.encrypt(rawPassword.toString()));
 	}
 }
 
